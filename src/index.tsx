4f66efab302d9a53c56052a18958644c7a70bb6c
Manish Trivedi <manish.tech2@gmail.com>
2022-09-08 15:51:44 +0530

    refactor: add android build.gradle and call in main method

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
+/* globals __scanPoseLandmarks */
+import type { Frame } from 'react-native-vision-camera';
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
 
 
 
-export function scanPoseLandmarkes(frame: Frame) {
+export function scanPoseLandmarks(frame: Frame) {
   'worklet';
-  return __scanPoseLandmarkes(frame);
+  // @ts-expect-error Frame Processors are not typed.
+  return __scanPoseLandmarks(frame);
 }

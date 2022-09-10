40851e9b9d96723e5a21c9d2a9ee0946fc9316d8
Manish Trivedi <manish.tech2@gmail.com>
2022-09-11 02:51:36 +0530

    chore: websocket integrated successfully

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
-import { StyleSheet, View, Text, } from 'react-native';
-// import { Camera, Frame, useCameraDevices, useFrameProcessor } from 'react-native-vision-camera';
+import { StyleSheet, View, Text } from 'react-native';
 
-import { RequestCameraPermission, Assessment} from 'xtravision-react-native';
-import { CameraPermissionStatus} from 'xtravision-react-native';
-
+import { RequestCameraPermission, Assessment } from 'xtravision-react-native';
+import { CameraPermissionStatus } from 'xtravision-react-native';
 
 export default function App() {
-
   const [hasPermission, setHasPermission] = React.useState(false);
 
   React.useEffect(() => {
@@ -17,54 +14,45 @@ export default function App() {
     })();
   }, []);
 
-   // required prop:
-   const cameraPosition = "front";
+  const [inPose, setInPose] = React.useState(false);
+  const [repsCounter, setRepsCounter] = React.useState(0);
+  // required prop:
+  function onServerResponse(serverResponse: any): void {
+    if (serverResponse.errors.length){
+      console.error('Server Error Response:', serverResponse.errors);
+      return ;
+    }
+   
+    console.log('Server Data:', serverResponse.data);
 
+    setRepsCounter(serverResponse.data?.reps);
+    setInPose(serverResponse.data?.in_pose);
+  }
 
-    // function examplePlugin(frame: Frame): string[] {
-    //   'worklet';
-    //   if (!_WORKLET) throw new Error('examplePlugin must be called from a frame processor!');
-    
-    //   // @ts-expect-error because this function is dynamically injected by VisionCamera
-    //   return __example_plugin(frame, 'hello!', 'parameter2', true, 42, { test: 0, second: 'test' }, ['another test', 5]);
-    // }
-
-
-    // const frameProcessor = useFrameProcessor((frame) => {
-    //     'worklet';
-    //     // const pose: any = scanPoseLandmarks(frame);
-    //     // console.log("Pose", pose)
-    //     console.log('hurree')
-    
-    //     const a = examplePlugin(frame)
-    //     console.log(a);
-
-    //     // runOnJS(poseFrameHandler)(pose, frame);
-    //   }, []);
-
-    // // if no camera found (front or back)
-    // if (device == null) {   
-    //     return <Text style={{color: 'red',fontWeight: 'bold'}}>Unable to detect camera.</Text>
-    // }
-
- 
+  const authToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkOTU1NTVkNS0wNmFhLTExZWQtOGJkYy0xMmZhYjRmZmFiZWQiLCJhcHBJZCI6IjY5YTdmMmU2LTA2YWEtMTFlZC04YmRjLTEyZmFiNGZmYWJlZCIsIm9yZ0lkIjoiNmQ5MWZlN2YtMDZhOS0xMWVkLThiZGMtMTJmYWI0ZmZhYmVkIiwiaWF0IjoxNjYwMTA3MjI0LCJleHAiOjE2OTE2NjQ4MjR9._i4MJbwPznHzxoStcRAcK7N7k_xGdUjvKwmHXv1zixM';
+  const assessmentName = 'SQUATS';
+  const cameraPosition = 'back';
 
   return (
-    <View  style={styles.container}> 
-      { hasPermission ? 
-        (
-          <>
-            <Text>App has Permission</Text>     
-            <Assessment 
-              cameraPosition={cameraPosition}
-              />
-          </>
-        ) : (
-          <>
-            <Text>App don't have Permission</Text>
-          </>
-        )
-      }
+    <View style={styles.container}>
+      {hasPermission ? (
+        <>
+          {/* <Text>App has Permission</Text> */}
+          <Assessment
+            cameraPosition={cameraPosition}
+            connection={{ authToken, queryParams: {} }}
+            assessment={assessmentName}
+            isEducationScreen={false}
+            onServerResponse={onServerResponse}
+          />
+          <Text>In-Pose: {inPose} ; Reps Counter: {repsCounter}</Text>
+        </>
+      ) : (
+        <>
+          <Text>App don't have Permission</Text>
+        </>
+      )}
     </View>
   );
 }

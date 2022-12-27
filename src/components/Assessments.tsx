c2029f30066bf5d0b3176b0aebcd37cbb308ab68
Manish Trivedi <manish.tech2@gmail.com>
2022-12-27 16:20:34 +0530

    fix: update prod url

--- src/components/Assessments.tsx
+++ src/components/Assessments.tsx
@@ -39,9 +39,9 @@ export interface AssessmentProp {
   }
 }
 
-//  const WS_BASE_URL = 'wss://saasai.xtravision.ai/wss/v2';
+ const WS_BASE_URL = 'wss://saasai.xtravision.ai/wss/v2';
 // const WS_BASE_URL = 'wss://saasstagingai.xtravision.ai/wss/v2';
-const WS_BASE_URL = 'ws://localhost:8000/wss/v2';
+// const WS_BASE_URL = 'ws://localhost:8000/wss/v2';
 
 export function Assessment(props: AssessmentProp) {
   // const { width, height } = Dimensions.get('window');

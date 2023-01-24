3e8e2af29b43ee8d4e5b21a2742ff5271eb5f90d
Manish Trivedi <mManishTrivedi@users.noreply.github.com>
2023-01-24 16:43:01 +0530

    refactor: make demo app more user friendly, and add v-sit-n-react with +ive/-ive (#26)

--- example/src/Components/AssessmentList.tsx
+++ example/src/Components/AssessmentList.tsx
@@ -6,17 +6,19 @@ export const AssessmentList = (props: any) => {
 
   const data = [
     //{ key: "HALF_SQUAT", value: "HALF_SQUAT" },
-    { key: "PARTIAL_CURL_UP", value: "PARTIAL_CURL_UP" },
-    { key: "PUSH_UPS", value: "PUSH_UPS" },
-    { key: "PLATE_TAPPING_COORDINATION", value: "PLATE_TAPPING_COORDINATION" },
-    { key: "SIDE_FLAMINGO", value: "SIDE_FLAMINGO" },
-    { key: "SIT_UPS", value: "SIT_UPS" },
-    { key: "STANDING_BROAD_JUMP", value: "STANDING_BROAD_JUMP" },
-    { key: "V_SIT_AND_REACH", value: "V_SIT_AND_REACH" },
+    { value: "Partial Curl-Up", key: "PARTIAL_CURL_UP" },
+    { value: "Push Ups", key: "PUSH_UPS" },
+    { value: "Plate Tapping", key: "PLATE_TAPPING_COORDINATION" },
+    { value: "Side Flamingo", key: "SIDE_FLAMINGO" },
+    { value: "Sit Ups", key: "SIT_UPS" },
+    { value: "Standing Broad Jump", key: "STANDING_BROAD_JUMP" },
+    { value: "V Sit & Reach", key: "V_SIT_AND_REACH" },
+    { value: "V Sit & Reach(+/-)", key: "V_SIT_AND_REACH-POSITIVE_NEGATIVE" },
+
   ]
   
   // testing
-  const defaultOption = { key: "PARTIAL_CURL_UP", value: "PARTIAL_CURL_UP" };
+  const defaultOption =  data[0] //{ key: "PARTIAL_CURL_UP", value: "PARTIAL_CURL_UP" };
   
   return (
     <SelectList

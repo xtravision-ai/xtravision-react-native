8eb5d519946b7d9f87f4cafbde6044a0a2a7a36c
Gandhar P <gandhar.pednekar15@gmail.com>
2023-05-25 22:09:09 +0530

    default assessments added to sample app

--- example/src/Components/AssessmentList.tsx
+++ example/src/Components/AssessmentList.tsx
@@ -5,21 +5,17 @@ import { SelectList } from 'react-native-dropdown-select-list'
 export const AssessmentList = (props: any) => {
 
   const data = [
-    { key: "HALF_SQUAT", value: "HALF_SQUAT" },
-    { value: "Partial Curl-Up", key: "PARTIAL_CURL_UP" },
-    { value: "Push Ups", key: "PUSH_UPS" },
-    { value: "Plate Tapping", key: "PLATE_TAPPING_COORDINATION" },
-    { value: "Side Flamingo", key: "SIDE_FLAMINGO" },
-    { value: "Sit Ups", key: "SIT_UPS" },
-    { value: "Standing Broad Jump", key: "STANDING_BROAD_JUMP" },
-    { value: "V Sit & Reach", key: "V_SIT_AND_REACH" },
-    { value: "V Sit & Reach(+/-)", key: "V_SIT_AND_REACH-POSITIVE_NEGATIVE" },
-
+    { key: "HALF_SQUAT", value: "Half Squat" },
+    { key: "BANDED_ALTERNATING_DIAGNOLS", value: "Banded Diagonal" },
+    { key: "SIT_WALL", value: "Sit Wall" },
+    { key: "PUSH_UPS", value: "Push Ups" },
+    { key: "SIT_AND_REACH_T2", value: "V Sit and Reach" },
+    { key: "SIT_UPS_T2", value: "Sit Ups" }
   ]
-  
+
   // testing
   const defaultOption =  data[0] //{ key: "PARTIAL_CURL_UP", value: "PARTIAL_CURL_UP" };
-  
+
   return (
     <SelectList
       data={data}
